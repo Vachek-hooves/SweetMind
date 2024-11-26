a7e93f32be05f047f84a86de79a8be1e0bc7268c
Vachek-hooves <vachek0602@gmail.com>
2024-11-26 18:19:46 +0200

    create day quotes

---
+++ data/dayliQuotes.js
@@ -0,0 +1,16 @@
+export const dayliQuotes = [
+  {id: 1, quote: 'Every day brings new opportunities to shine.'},
+  {id: 2, quote: 'The only way to do great work is to love what you do.'},
+  {id: 3, quote: 'Believe you can and you\'re halfway there.'},
+  {id: 4, quote: 'Your positive action combined with positive thinking results in success.'},
+  {id: 5, quote: 'The future belongs to those who believe in the beauty of their dreams.'},
+  {id: 6, quote: 'Success is not final, failure is not fatal: it is the courage to continue that counts.'},
+  {id: 7, quote: 'Life is 10% what happens to you and 90% how you react to it.'},
+  {id: 8, quote: 'The only limit to our realization of tomorrow will be our doubts of today.'},
+  {id: 9, quote: 'What you get by achieving your goals is not as important as what you become by achieving your goals.'},
+  {id: 10, quote: 'The way to get started is to quit talking and begin doing.'},
+  {id: 11, quote: "Don't watch the clock; do what it does. Keep going."},
+  {id: 12, quote: "Everything you've ever wanted is on the other side of fear."},
+  {id: 13, quote: "The only person you are destined to become is the person you decide to be."},
+
+];
