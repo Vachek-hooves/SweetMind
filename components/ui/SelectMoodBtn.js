2b86a4820c06d1e5f15bde8f1ce5c4a087b2aac8
Vachek-hooves <vachek0602@gmail.com>
2024-11-26 23:18:26 +0200

    mood button emodji animated

--- components/ui/SelectMoodBtn.js
+++ components/ui/SelectMoodBtn.js
@@ -1,10 +1,45 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View,Animated} from 'react-native';
+import {useRef, useEffect} from 'react';
 import {getBtnEmodji} from '../../data/btnEmodji';
 
+
 const SelectMoodBtn = ({mood, onPress}) => {
+    const floatAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const floatingAnimation = Animated.loop(
+        Animated.sequence([
+          Animated.timing(floatAnim, {
+            toValue: 1,
+            duration: 2000,
+            useNativeDriver: true,
+          }),
+          Animated.timing(floatAnim, {
+            toValue: 0,
+            duration: 2000,
+            useNativeDriver: true,
+          }),
+        ])
+      );
+  
+      floatingAnimation.start();
+  
+      // Cleanup animation on component unmount
+      return () => floatingAnimation.stop();
+  }, []);
+
+  const yOffset = floatAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0,-10],
+  });
+
+
   return (
     <TouchableOpacity style={styles.moodButton} onPress={() => onPress(mood)}>
+        <Animated.View style={{transform: [{translateY: yOffset}]}}>
+
       <View>{getBtnEmodji(mood)}</View>
+        </Animated.View>
       <Text style={styles.moodText}>
         {mood.charAt(0).toUpperCase() + mood.slice(1)}
       </Text>
