47bf5300668786ad2a7995f22e0d2208e486376b
Vachek-hooves <vachek0602@gmail.com>
2025-09-02 13:26:36 +0300

    done

---
+++ config/credentials.js
@@ -0,0 +1,29 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'pCY43sbDhDN9SBrRP7iQf9',
+  // appId: '',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key - 6516e978c4d9fb1e4271e0a06652dbc7
+
+// FB app cred
+export const FB_APP_ID = '751400911043091';
+export const FB_CLIENT_TOKEN = '0f2f9eed24b8277d3fc2a8d960de0d00';
+
+// Compaign
+// https://bright-base-link.space/dEl1kLgR
+export const INITIAL_URL = `https://bright-base-link.space/`;
+export const URL_IDENTIFAIRE = `dEl1kLgR`;
+
+// one signal key ex 3553 Holland Tulip Quiz
+export const ONE_SIGNAL_ID = '33bdc2c3-7949-42bf-8a90-0c5ebe374e58';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - 42a82801-2171-4193-b134-a6c361bbb6d2
